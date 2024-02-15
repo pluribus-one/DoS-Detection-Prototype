68ac046e242758d32072b4c2bd2f3d7a1afc2ee2
Marco Ledda <m.ledda45@studenti.unica.it>
2024-02-15 11:51:16 +0100

    Update offline model

--- offline-model-implementation/cvn.py
+++ offline-model-implementation/cvn.py
@@ -22,48 +22,32 @@ class CVN(BaseEstimator, ClassifierMixin):
     
         derivative          = np.diff(y) / np.diff(x)
         masked_derivative   = np.ma.masked_equal(derivative, 0)
-        #masked_derivative   = np.ma.masked_where(derivative >= 1.0e+2, derivative)
         min                 = np.min(masked_derivative)
+        min_idx             = np.where(derivative == min)[0][0]
 
-        #min_coords          = np.unravel_index(np.argmin(masked_derivative), masked_derivative.shape)
+        self.plateau_coord.append((x[min_idx], y[min_idx]))
 
-        print(min)
-        i = np.where(derivative == min)[0][0]
-
-        print((x[i], y[i]))
-
-        # for i in range(len(derivative)):
-        #     if abs(derivative[i]) < self.threshold:
-        #         if i + min_length < len(derivative) and all(abs(derivative[j]) < self.threshold for j in range(i, i + min_length)):
-        #             self.plateau_coord.clear()
-        #             plateau_x = set_x[i + min_length // 2]
-        #             plateau_y = set_y_perc[i + min_length // 2]
-        #             self.plateau_coord.append(plateau_x)
-        #             self.plateau_coord.append(plateau_y)
-        
-        # for i in range(0, len(self.plateau_coord), 2):
-        #     current_couple = self.plateau_coord[i:i+2]
-        #     plt.plot(current_couple[0], current_couple[1], marker='o', markersize=5, color='red', label='Plateau')
-    
         plt.plot(x, y)
+        plt.plot(x[min_idx], y[min_idx], marker='o', markersize=5, color='red', label='Plateau')
         plt.grid()
         plt.title("Probability p(n <= N) = fraction of clients generating max N requests")
-        plt.xlabel("Numer of requests N")
+        plt.xlabel("Number of requests N")
         plt.ylabel("p(n<=N) %")
+        plt.savefig(f"plot_{min_idx}.pdf")
         plt.show()
-        plt.savefig("plot.pdf")
-
-
+        
     def _compute_windows(self, X):
         """
-        TODO: Add description
+        Compute the windows performing a right shift on the unix timestamp.
+
+                Parameters:
+                        X (): Dataset of requests made up of timestamp, ip address and unix timestamp.
         """
         for _, row in X.iterrows():
             ip = row['ip']
             for window_idx in range(1, self._num_windows + 1):
                 window = shift_right(row['unix_timestamp'], window_idx)
                 self._count[window][ip] += 1
-
 
     def _aggregate_data(self):
         """
@@ -81,7 +65,6 @@ class CVN(BaseEstimator, ClassifierMixin):
         for window, ip_metrics in self._count.items():
             # Remove Ni duplicated
             n_requests = list(dict.fromkeys(ip_metrics.values()))
-            # TODO: Order is required?
             n_requests.sort()
 
             match 10 - len(str(window)):
@@ -94,7 +77,6 @@ class CVN(BaseEstimator, ClassifierMixin):
                 case 3: # Window 3
                     self._ip_metrics_for_window[2] = merge_dicts(self._ip_metrics_for_window[2], ip_metrics)
                     self._n_requests_for_window[2] = list(set(self._n_requests_for_window[2] + n_requests))
-
 
     def _calculate_cumulative_probabilities(self):
         """
@@ -111,26 +93,17 @@ class CVN(BaseEstimator, ClassifierMixin):
 
                 prob_cumulative.append(prob)
 
-            # Computing: 
-            # diffs = [
-            #     (prob_cumulative[i+1] - prob_cumulative[i]) /
-            #     ((self._n_requests_for_window[window_idx])[i+1] - (self._n_requests_for_window[window_idx])[i])
-            #     for i in range(len(self._n_requests_for_window[window_idx])-1)
-            # ]
-
-            # print(self._n_requests_for_window[window_idx][diffs.index(min(diffs))])
-
-            # self._Nts[window_idx] = self._n_requests_for_window[window_idx][diffs.index(min(diffs))]
-
-            # Ni_list_plot[window_idx].extend(total_ni_s[window_idx])
-            # prob_list_plot[window_idx].extend(prob_cumulative)
-
-            self.plateau_coord.append(self._find_plateau(window_idx, prob_cumulative))
-
+            self._find_plateau(window_idx, prob_cumulative)
 
     def fit(self, X):
         """
-        TODO: Add description
+        Fitting algorithm. It learns the Nt thresholds for each window driven by the data.
+
+                Parameters:
+                        X (): Dataset of requests made up of timestamp, ip address and unix timestamp.
+
+                Returns:
+                        self
         """
 
         self._compute_windows(X)
--- offline-model-implementation/main.py
+++ offline-model-implementation/main.py
@@ -2,7 +2,7 @@ from cvn import CVN
 import pandas as pd
 from utils import to_unix_timestamp
 
-df = pd.read_csv('features.csv', nrows=500_000)
+df = pd.read_csv('features.csv', nrows=5_000_000)
 df['unix_timestamp'] = df['timestamp'].apply(to_unix_timestamp)
 
 clf = CVN()
